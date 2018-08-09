ba5144c679565330be512354331a04315d18fbca
Elias Mina <hukitan@hotmail.com>
2018-08-09 14:15:45 -0500

    Se realizan las modificaciones en las graficas solicitadas por AR: - No titulos de grafica - En la grafica de tareas se quitan los rotulos pues ya estan en el eje - Se recorta el area de visualización a 50(500)-100(1000) para apreciar mejor las diferencias - Se dejan de generar las graficas para d prima, quedan comentadas por cualquier cosa

--- Grafica.R
+++ Grafica.R
@@ -11,15 +11,10 @@ summarySE(wm_RC, measurevar = "value", groupvars = c("Tarea", "Dificultad")) %>%
     size = .9
   ) +
   xlab("Tarea") +
-  ylab("Porcentaje ± sd") +
-  scale_fill_hue(
-    name = "Tarea", # Legend label, use darker colors
-    breaks = c("Mt", "Mp"),
-    labels = c("Mantenimiento", "Manipulación")
-  ) +
-  ggtitle("Porcentaje de respuestas correctas\n por tareas") +
-  ylim(c(0, 100)) +
+  ylab("Porcentaje de respuestas correctas \n Media ± EEM") +
+  coord_cartesian(ylim = c(50,100)) +
   theme_minimal() +
+  theme(legend.position="none") +
   ggsave("graf/tarea.png", height = 3, width = 5)
 
 ##### Grafica por tarea x turno -------------
@@ -32,14 +27,13 @@ summarySE(wm_RC, measurevar = "value", groupvars = c("Tarea", "Dificultad", "Tur
     position = position_dodge(.9)
   ) +
   xlab("Tarea") +
-  ylab("Porcentaje ± sd") +
+  ylab("Porcentaje de respuestas correctas\n Media ± EEM") +
   scale_fill_hue(
     name = "Turno", # Legend label, use darker colors
     breaks = c("1", "2", "3"),
     labels = c("Mañana", "Tarde", "Noche")
   ) +
-  ggtitle("Porcentaje de respuestas correctas\n por turno y tarea") +
-  ylim(c(0, 100)) +
+  coord_cartesian(ylim = c(50,100)) +
   theme_minimal() +
   ggsave("graf/tarxtur.png", height = 3, width = 5)
 ##### Grafica por turno -------------
@@ -52,14 +46,13 @@ summarySE(wm_RC, measurevar = "value", groupvars = c("Dificultad", "Turno")) %>%
     position = position_dodge(.9)
   ) +
   xlab("Turno") +
-  ylab("Porcentaje ± sd") +
+  ylab("Porcentaje de respuestas correctas\n Media ± EEM") +
   scale_fill_hue(
     name = "Turno", # Legend label, use darker colors
     breaks = c("1", "2", "3"),
     labels = c("Mañana", "Tarde", "Noche")
   ) +
-  ggtitle("Porcentaje de respuestas correctas\n por turno") +
-  ylim(c(0, 100)) +
+  coord_cartesian(ylim = c(50,100)) +
   theme_minimal() +
   theme(axis.ticks.x = element_blank(), axis.text.x = element_blank()) +
   ggsave("graf/turno.png", height = 3, width = 5)
@@ -74,15 +67,10 @@ summarySE(wm_RT, measurevar = "value", groupvars = c("Tarea", "Dificultad")) %>%
     size = .9
   ) +
   xlab("Tarea") +
-  ylab("ms±sd") +
-  scale_fill_hue(
-    name = "Tarea", # Legend label, use darker colors
-    breaks = c("Mt", "Mp"),
-    labels = c("Mantenimiento", "Manipulación")
-  ) +
-  ggtitle("Tiempo de reacción\n por tareas") +
-  ylim(c(0, 1000)) +
+  ylab("Tiempo de reacción (ms)\n Media ± EEM") +
+    coord_cartesian(ylim = c(500,1000)) +
   theme_minimal() +
+  theme(legend.position="none") +
   ggsave("graf/tareaRT.png", height = 3, width = 5)
 
 ##### Grafica RT por tarea x turno -------------
@@ -95,14 +83,13 @@ summarySE(wm_RT, measurevar = "value", groupvars = c("Tarea", "Dificultad", "Tur
     position = position_dodge(.9)
   ) +
   xlab("Tarea") +
-  ylab("ms±sd") +
+  ylab("Tiempo de reacción (ms)\n Media ± EEM") +
   scale_fill_hue(
     name = "Turno", # Legend label, use darker colors
     breaks = c("1", "2", "3"),
     labels = c("Mañana", "Tarde", "Noche")
   ) +
-  ggtitle("Tiempo de reacción\n por turno y tarea") +
-  ylim(c(0, 1000)) +
+  coord_cartesian(ylim = c(500,1000)) +
   theme_minimal() +
   ggsave("graf/tarxturRT.png", height = 3, width = 5)
 ##### Grafica RC por turno -------------
@@ -115,74 +102,73 @@ summarySE(wm_RT, measurevar = "value", groupvars = c("Dificultad", "Turno")) %>%
     position = position_dodge(.9)
   ) +
   xlab("Turno") +
-  ylab("ms±sd") +
+  ylab("Tiempo de reacción (ms)\n Media ± EEM") +
   scale_fill_hue(
     name = "Turno", # Legend label, use darker colors
     breaks = c("1", "2", "3"),
     labels = c("Mañana", "Tarde", "Noche")
   ) +
-  ggtitle("Tiempo de reacción\n por turno") +
-  ylim(c(0, 1000)) +
+  coord_cartesian(ylim = c(500,1000)) +
   theme_minimal() +
   theme(axis.ticks.x = element_blank(), axis.text.x = element_blank()) +
   ggsave("graf/turnoRT.png", height = 3, width = 5)
 
-###### Graficas  d prima-----------
-
-summarySE(wm_Dprim, measurevar = "value", groupvars = c("Tarea", "Dificultad")) %>%
-  ggplot(aes(x = factor(Tarea), y = value, fill = Tarea)) +
-  geom_bar(position = position_dodge(), stat = "identity") +
-  geom_errorbar(aes(ymin = value - se, ymax = value + se),
-    width = .4,
-    size = .9
-  ) +
-  xlab("Tarea") +
-  ylab("d'±sd") +
-  scale_fill_hue(
-    name = "Tarea", # Legend label, use darker colors
-    breaks = c("Mt", "Mp"),
-    labels = c("Mantenimiento", "Manipulación")
-  ) +
-  ggtitle("Indice d'\n por tareas") +
-  theme_minimal() +
-  ggsave("graf/tareaDprim.png", height = 3, width = 5)
-
-##### Grafica RT por tarea x turno -------------
-summarySE(wm_Dprim, measurevar = "value", groupvars = c("Tarea", "Dificultad", "Turno")) %>%
-  ggplot(aes(x = factor(Tarea), y = value, fill = Turno)) +
-  geom_bar(position = position_dodge(), stat = "identity") +
-  geom_errorbar(aes(ymin = value - se, ymax = value + se),
-    width = .4,
-    size = .9,
-    position = position_dodge(.9)
-  ) +
-  xlab("Tarea") +
-  ylab("d'±sd") +
-  scale_fill_hue(
-    name = "Turno", # Legend label, use darker colors
-    breaks = c("1", "2", "3"),
-    labels = c("Mañana", "Tarde", "Noche")
-  ) +
-  ggtitle("Indice d´\n por turno y tarea") +
-  theme_minimal() +
-  ggsave("graf/tarxturDprim.png", height = 3, width = 5)
-##### Grafica RC por turno -------------
-summarySE(wm_Dprim, measurevar = "value", groupvars = c("Dificultad", "Turno")) %>%
-  ggplot(aes(x = factor(Turno), y = value, fill = Turno)) +
-  geom_bar(position = position_dodge(), stat = "identity") +
-  geom_errorbar(aes(ymin = value - se, ymax = value + se),
-    width = .4,
-    size = .9,
-    position = position_dodge(.9)
-  ) +
-  xlab("Turno") +
-  ylab("d'±sd") +
-  scale_fill_hue(
-    name = "Turno", # Legend label, use darker colors
-    breaks = c("1", "2", "3"),
-    labels = c("Mañana", "Tarde", "Noche")
-  ) +
-  ggtitle("Indice d´\n por turno") +
-  theme_minimal() +
-  theme(axis.ticks.x = element_blank(), axis.text.x = element_blank()) +
-  ggsave("graf/turnoDprim.png", height = 3, width = 5)
+# ###### Graficas  d prima-----------
+# 
+# summarySE(wm_Dprim, measurevar = "value", groupvars = c("Tarea", "Dificultad")) %>%
+#   ggplot(aes(x = factor(Tarea), y = value, fill = Tarea)) +
+#   geom_bar(position = position_dodge(), stat = "identity") +
+#   geom_errorbar(aes(ymin = value - se, ymax = value + se),
+#     width = .4,
+#     size = .9
+#   ) +
+#   xlab("Tarea") +
+#   ylab("d'±sd") +
+#   scale_fill_hue(
+#     name = "Tarea", # Legend label, use darker colors
+#     breaks = c("Mt", "Mp"),
+#     labels = c("Mantenimiento", "Manipulación")
+#   ) +
+#   ggtitle("Indice d'\n por tareas") +
+#   theme_minimal() +
+#   ggsave("graf/tareaDprim.png", height = 3, width = 5)
+# 
+# ##### Grafica RT por tarea x turno -------------
+# summarySE(wm_Dprim, measurevar = "value", groupvars = c("Tarea", "Dificultad", "Turno")) %>%
+#   ggplot(aes(x = factor(Tarea), y = value, fill = Turno)) +
+#   geom_bar(position = position_dodge(), stat = "identity") +
+#   geom_errorbar(aes(ymin = value - se, ymax = value + se),
+#     width = .4,
+#     size = .9,
+#     position = position_dodge(.9)
+#   ) +
+#   xlab("Tarea") +
+#   ylab("d'±sd") +
+#   scale_fill_hue(
+#     name = "Turno", # Legend label, use darker colors
+#     breaks = c("1", "2", "3"),
+#     labels = c("Mañana", "Tarde", "Noche")
+#   ) +
+#   ggtitle("Indice d´\n por turno y tarea") +
+#   theme_minimal() +
+#   ggsave("graf/tarxturDprim.png", height = 3, width = 5)
+# ##### Grafica RC por turno -------------
+# summarySE(wm_Dprim, measurevar = "value", groupvars = c("Dificultad", "Turno")) %>%
+#   ggplot(aes(x = factor(Turno), y = value, fill = Turno)) +
+#   geom_bar(position = position_dodge(), stat = "identity") +
+#   geom_errorbar(aes(ymin = value - se, ymax = value + se),
+#     width = .4,
+#     size = .9,
+#     position = position_dodge(.9)
+#   ) +
+#   xlab("Turno") +
+#   ylab("d'±sd") +
+#   scale_fill_hue(
+#     name = "Turno", # Legend label, use darker colors
+#     breaks = c("1", "2", "3"),
+#     labels = c("Mañana", "Tarde", "Noche")
+#   ) +
+#   ggtitle("Indice d´\n por turno") +
+#   theme_minimal() +
+#   theme(axis.ticks.x = element_blank(), axis.text.x = element_blank()) +
+#   ggsave("graf/turnoDprim.png", height = 3, width = 5)
